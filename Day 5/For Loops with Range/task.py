109cf98abcd22a8a3dec1fe3818f8211babf19dd
Michael Shanks <mshan62@wgu.edu>
2026-08-29 15:28:18 -0500

    Completed HighScore and ForLoop

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,7 @@
+# Using range function has to be used in conjuction with loop
+
+# range() anatomy range(start, stop, step)
+# the stop parameter will not include last int (must extend stop num
+# beyond the expected end).
+for number in range(1, 11):
+    print(number)
--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,7 @@
 fruits = ["Apple", "Peach", "Pear"]
+# access list, and assign item to variable and print each item each cycle.
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie") # concatenate pie to each item
+
+print(fruits) # prints outside the loop
